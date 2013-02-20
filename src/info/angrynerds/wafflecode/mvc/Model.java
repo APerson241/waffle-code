083583cb14e1ac60ee4cc9b2028396cbb90a4f0f
Daniel Glus <setup.pyc@gmail.com>
2013-02-19 22:41:52 -0500

    Moved everything from the Dropbox folder over
    It was a lot of stuff.

--- src/info/angrynerds/wafflecode/mvc/Model.java
+++
@@ -1,6 +0,0 @@
-package info.angrynerds.wafflecode.mvc;
-
-public interface Model {
-	public String getTitleOfProgram();
-	public void setTitleOfProgram();
-}
--- src/info/angrynerds/wafflecode/mvc/View.java
+++
@@ -1,5 +0,0 @@
-package info.angrynerds.wafflecode.mvc;
-
-public interface View {
-	public void setVisible(boolean visible);
-}
